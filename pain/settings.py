6f5d8a8c7c64a66a25fd3631041fb59bed43bce1
john <buzmakoff2005@mail.ru>
2020-06-20 17:13:49 +0300

    initi commit

--- pain/settings.py
+++ pain/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uu1_q8_qe5_w0csmm(pxqr^su_f$cxe!mmj=zx4dn5$zlse@ei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
